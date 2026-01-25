c0e0b8f00b95185edcc607c1d1498c647a5af762
Thor23-web <subhankar282005@gmail.com>
2026-01-25 21:32:02 +0530

    Refactor insertion sort implementation: improve code formatting for better readability

--- 02-sorting/insertion.cpp
+++ 02-sorting/insertion.cpp
@@ -1,26 +1,32 @@
-#include<iostream>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-void insertion(int arr[],int x){
-    for (int i =0 ; i<=x;i++){
-        int j =i;
-        while(j>0 && arr[j-1]>arr[j]){
-         swap(arr[j-1],arr[j]);
-         j--;
+void insertion(int arr[], int x)
+{
+    for (int i = 0; i <= x; i++)
+    {
+        int j = i;
+        while (j > 0 && arr[j - 1] > arr[j])
+        {
+            swap(arr[j - 1], arr[j]);
+            j--;
         }
     }
 }
 
-int main(){
-        int n;
-    cin>>n;
+int main()
+{
+    int n;
+    cin >> n;
     int arr[n];
-    for(int i =0; i<n;i++){
-        cin>>arr[i];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
     }
-     insertion(arr,n);
-     for(int i =0; i<n;i++){
-        cout<<arr[i]<<" ";
+    insertion(arr, n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
     }
     return 0;
 }
