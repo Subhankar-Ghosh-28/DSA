a7f535eec448dd188963365dca78a2e4ec6182da
Thor23-web <subhankar282005@gmail.com>
2026-02-07 22:48:47 +0530

    fix(binarySearch): correct high index initialization in search insert position function

--- 06-BinarySearch/04-searchInsertposition.cpp
+++ 06-BinarySearch/04-searchInsertposition.cpp
@@ -3,7 +3,7 @@ using namespace std;
 //search for the index of the target value in the sorted array.
 
 int inssertPosition(vector<int> &arr, int k){
-    int low = 0, high = arr.size();
+    int low = 0, high = arr.size() - 1;
     int ans = arr.size();
     while(low<= high){
         int mid = low + (high - low)/2;
