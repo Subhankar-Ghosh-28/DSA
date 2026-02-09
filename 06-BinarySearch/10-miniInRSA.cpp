48f900c183ec9ec9c6d7349711188f22dd2797f0
Thor23-web <subhankar282005@gmail.com>
2026-02-10 00:28:23 +0530

    refactor(minimumElement): simplify brute force and optimal methods by removing unused parameters

--- 06-BinarySearch/10-miniInRSA.cpp
+++ 06-BinarySearch/10-miniInRSA.cpp
@@ -6,7 +6,7 @@ using namespace std;
 class Solution
 {
 public:
-    int bruteForce(vector<int> &nums, int k)
+    int bruteForce(vector<int> &nums)
     {
         int mini = INT_MIN;
         for (int i = 1; i < nums.size(); i++)
@@ -19,13 +19,37 @@ public:
         // TC -> O(N);
     }
 
-    int optimal(vector<int> &nums, int k)
+    int optimal(vector<int> &nums)
     {
+        int n = nums.size();
+        int low = 0, high = n - 1;
+
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] > nums[high])
+            {
+                low = mid + 1;
+            }
+
+            else
+                high = mid;
+        }
+
+        return nums[low];
     }
 };
 
 int main()
 {
+    vector<int> nums = {4, 5, 6, 7, 0, 1, 2};
+
+    Solution sol;
+
+    int result = sol.optimal(nums);
+
+    cout << "Minimum element is " << result << endl;
 
     return 0;
 }
