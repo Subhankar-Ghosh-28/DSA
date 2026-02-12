dc8ed75c088af21574822bf7d7adcad489b161cf
Thor23-web <subhankar282005@gmail.com>
2026-02-12 11:12:33 +0530

    feat(Set_matrix_zero): implement optimized zeroing of matrix rows and columns

--- 04-array/22-Set_matrix_zero.cpp
+++ 04-array/22-Set_matrix_zero.cpp
@@ -60,10 +60,38 @@ public:
                 }
             }
         }
+
+        //TC -> O(m * n * (m + n)));
     }
 
-    vector<vector<int>> better(vector<vector<int>> &matrix)
+    void better(vector<vector<int>> &matrix)
     {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
+
+        // First pass: mark rows and columns that need to be zeroed
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // If element is zero, mark its row and column
+                if (matrix[i][j] == 0) {
+                    row[i] = 1;
+                    col[j] = 1;
+                }
+            }
+        }
+
+        // Second pass: set cells to zero based on markers
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                // If the row or column is marked, set cell to zero
+                if (row[i] == 1 || col[j] == 1) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
     }
 
     vector<vector<int>> optimal(vector<vector<int>> &matrix)
@@ -77,7 +105,7 @@ int main()
 
     Solution sol;
 
-    sol.bruteForce(matrix);
+    sol.better(matrix);
 
     for (auto x : matrix)
     {
