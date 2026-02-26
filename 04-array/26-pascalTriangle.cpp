54591555ccebb75a9facdb40cbe10d2fc0eac0af
Thor23-web <subhankar282005@gmail.com>
2026-02-26 23:44:07 +0530

    refactor(pascalTriangle): rename parameter for clarity and improve code structure

--- 04-array/26-pascalTriangle.cpp
+++ 04-array/26-pascalTriangle.cpp
@@ -3,14 +3,13 @@ using namespace std;
 
 class Solution1
 {
-
 public:
     // Generate full triangle
-    vector<vector<int>> generator(int rowNum)
+    vector<vector<int>> generator(int n)
     {
         vector<vector<int>> triangle;
 
-        for (int i = 0; i < rowNum; i++)
+        for (int i = 0; i < n; i++)
         {
 
             vector<int> row(i + 1, 1);
@@ -22,6 +21,37 @@ public:
             triangle.push_back(row);
         }
         return triangle;
+
+        // TC -> O(N*N); sc -> O(N*N)
+    }
+
+    vector<long long> onlyOneRow(int N)
+    {
+        vector<long long> row;
+        long long val = 1;
+        row.push_back(val);
+        for (int i = 1; i < N; i++)
+        {
+            val = val * (N - i) / i;
+            row.push_back(val);
+        }
+        return row;
+
+        // TC -> O(N); sc -> O(N)
+    }
+
+    long long findPascalElement(int r, int c)
+    {
+        int n = r - 1;
+        int k = c - 1;
+
+        long long result = 1;
+        for (int i = 0; i < k; i++)
+        {
+            result *= (n - i);
+            result /= (i + 1);
+        }
+        return result;
     }
 };
 
@@ -40,5 +70,18 @@ int main()
         }
         cout << "\n";
     }
+    cout << "\n";
+
+    int N = 5;
+    vector<long long> row = sol.onlyOneRow(N);
+    cout << "ith row is :" << endl;
+    for (auto it : row)
+    {
+        cout << it << " ";
+    }
+    cout << "\n";
+
+    int r = 5, c = 3;
+    cout << "The element at row " << r << " col " << c << " is : " << sol.findPascalElement(r, c);
     return 0;
 }
