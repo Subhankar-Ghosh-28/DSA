f3e585cbbf01a761aee93b50e7ad2f7ca6d2e7fd
Thor23-web <subhankar282005@gmail.com>
2026-03-28 19:09:23 +0530

    feat(binaryTree): add level order traversal implementation and update main function to display results

--- 13-BinaryTree/01-Traversal.cpp
+++ 13-BinaryTree/01-Traversal.cpp
@@ -51,6 +51,49 @@ public:
     }
 };
 
+class LevelOrderTravarse
+{
+public:
+    vector<vector<int>> LevelOrder(Node *root)
+    {
+        vector<vector<int>> ans;
+
+        if (root == NULL)
+            return ans;
+
+        queue<Node *> q;
+
+        q.push(root);
+
+        while (!q.empty())
+        {
+            int size = q.size();
+
+            vector<int> level;
+
+            for (int i = 0; i < size; i++)
+            {
+                Node *node = q.front();
+                q.pop();
+
+                level.push_back(node->data);
+
+                if (node->left != nullptr)
+                {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr)
+                {
+                    q.push(node->right);
+                }
+            }
+
+            ans.push_back(level);
+        }
+        return ans;
+    }
+};
+
 int main()
 {
 
@@ -77,6 +120,21 @@ int main()
     cout << "Post-order traversal : ";
     sol.PostOrder(root);
     cout << endl;
+
+    LevelOrderTravarse solu;
+
+    vector<vector<int>> result = solu.LevelOrder(root);
+
+    cout << "Level Order Traversal of Tree: ";
+
+    for (auto x : result)
+    {
+        for (auto y : x)
+        {
+            cout << y << " ";
+        }
+        cout<<endl;
+    }
 
     return 0;
 }
