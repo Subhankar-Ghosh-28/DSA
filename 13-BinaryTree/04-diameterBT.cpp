301dd66b4727b335d7889076a1a7bf97871bebee
Thor23-web <subhankar282005@gmail.com>
2026-04-03 11:08:41 +0530

    feat(diameterBT): add function to retrieve the longest path for diameter calculation in binary tree

--- 13-BinaryTree/04-diameterBT.cpp
+++ 13-BinaryTree/04-diameterBT.cpp
@@ -60,11 +60,61 @@ public:
         int lh = optimal(root->left, diameter);
         int rh = optimal(root->right, diameter);
 
-        diameter = max(diameter, lh + rh);
+        diameter = max(diameter, lh + rh); // count edge
 
         return max(lh, rh) + 1;
 
         // TC-> O(N);
+    }
+
+    vector<int> bestPath;
+
+    // Function returns the longest downward path from root
+    vector<int> solve(Node *root, int &diameter)
+    {
+        if (!root)
+            return {};
+
+        vector<int> left = solve(root->left, diameter);
+        vector<int> right = solve(root->right, diameter);
+
+        // Current diameter (nodes count)
+        int currDiameter = left.size() + right.size() + 1;
+
+        if (currDiameter > diameter)
+        {
+            diameter = currDiameter;
+
+            bestPath.clear();
+
+            // left path (reverse)
+            for (int i = left.size() - 1; i >= 0; i--)
+            {
+                bestPath.push_back(left[i]);
+            }
+
+            // root
+            bestPath.push_back(root->data);
+
+            // right path
+            for (int x : right)
+            {
+                bestPath.push_back(x);
+            }
+        }
+
+        if (left.size() > right.size())
+        {
+            vector<int> temp = left;
+            temp.insert(temp.begin(), root->data); // root at front
+            return temp;
+        }
+        else
+        {
+            vector<int> temp = right;
+            temp.insert(temp.begin(), root->data);
+            return temp;
+        }
     }
 };
 
@@ -75,8 +125,6 @@ int main()
     root->right = new Node(3);
     root->left->left = new Node(4);
     root->left->right = new Node(5);
-    root->left->right->right = new Node(6);
-    root->left->right->right->right = new Node(7);
 
     Solution solution;
 
@@ -86,6 +134,14 @@ int main()
 
     solution.optimal(root, diameter);
 
-    cout << "The diameter of the binary tree is: " << diameter << endl;
+    cout << "The diameter of the binary tree is (edge): " << diameter << endl;
+
+    solution.solve(root, diameter);
+    cout << "Diameter path: ";
+    for (int x : solution.bestPath)
+    {
+        cout << x << " ";
+    }
+    cout << endl;
     return 0;
 }
