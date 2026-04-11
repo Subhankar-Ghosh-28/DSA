87f2c8ba1f13343d6a7b5fe12cd825ae7275cc3f
Thor23-web <subhankar282005@gmail.com>
2026-04-11 20:43:04 +0530

    fix(Solution): correct access modifier for Solve method and enhance main function for testing

--- 13-BinaryTree/12-pathToNode.cpp
+++ 13-BinaryTree/12-pathToNode.cpp
@@ -38,7 +38,8 @@ bool getPath(Node *root, vector<int> &arr, int x)
 
 class Solution
 {
-    vector<int> Solution::Solve(Node *root, int b)
+public:
+    vector<int> Solve(Node *root, int b)
     {
         vector<int> ans;
 
@@ -55,6 +56,33 @@ class Solution
 
 int main()
 {
+    Node *root = new Node(3);
+    root->left = new Node(5);
+    root->right = new Node(1);
+    root->left->left = new Node(6);
+    root->left->right = new Node(2);
+    root->right->left = new Node(0);
+    root->right->right = new Node(8);
+    root->left->right->left = new Node(7);
+    root->left->right->right = new Node(4);
 
+    Solution sol;
+
+    int targetLeafValue = 7;
+
+    vector<int> path = sol.Solve(root, targetLeafValue);
+
+    // Print the path
+    cout << "Path from root to node with value ";
+    cout << targetLeafValue << ": ";
+
+    for (int i = 0; i < path.size(); ++i)
+    {
+        cout << path[i];
+        if (i < path.size() - 1)
+        {
+            cout << " -> ";
+        }
+    }
     return 0;
 }
