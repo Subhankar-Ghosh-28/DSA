b5e6f8f6340b1d06ba066efe625d288b502d6c09
Thor23-web <subhankar282005@gmail.com>
2026-04-11 19:41:42 +0530

    style: improve code formatting and consistency in pathToNode.cpp

--- 13-BinaryTree/12-pathToNode.cpp
+++ 13-BinaryTree/12-pathToNode.cpp
@@ -1,44 +1,51 @@
 // print root to give leaf node path in BT
 
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Node {
+class Node
+{
 public:
     int data;
-    Node* left;
-    Node* right;
+    Node *left;
+    Node *right;
 
     Node(int key)
     {
-       data = key;
-       left = nullptr;
-       right = nullptr;
+        data = key;
+        left = nullptr;
+        right = nullptr;
     }
-
 };
 
-    bool getPath(Node* root, vector<int> &arr, int x){
-        if(root == NULL) return false;
-
-        arr.push_back(root->data);
-
-        if(root->data == x) return true;
-
-        if(getPath(root->left, arr, x) || getPath(root->right, arr, x)) return true;
-
-        arr.pop_back();
-
+bool getPath(Node *root, vector<int> &arr, int x)
+{
+    if (root == NULL)
         return false;
-    }
 
-class Solution{
-    vector<int> Solution::Solve(Node* root, int b){
+    arr.push_back(root->data);
+
+    if (root->data == x)
+        return true;
+
+    if (getPath(root->left, arr, x) || getPath(root->right, arr, x))
+        return true;
+
+    arr.pop_back();
+
+    return false;
+}
+
+class Solution
+{
+    vector<int> Solution::Solve(Node *root, int b)
+    {
         vector<int> ans;
 
-        if(root == NULL) return ans;
+        if (root == NULL)
+            return ans;
 
-        getPath(root,ans,b);
+        getPath(root, ans, b);
 
         return ans;
 
@@ -46,7 +53,8 @@ class Solution{
     }
 };
 
-int main(){
+int main()
+{
 
-return 0;
+    return 0;
 }
