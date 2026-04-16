9efebfff52f9e0a31116919ef94ce50645447498
Thor23-web <subhankar282005@gmail.com>
2026-04-17 00:58:44 +0530

    feat(PrintLinkedList): add implementation to print elements of a linked list

---
+++ 07-linkedlist/03-printLL.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+public:
+    Node(int data1, Node *next1)
+    {
+        data = data1;
+        next = next1;
+    }
+
+public:
+    Node(int data1)
+    {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+void print(Node *head)
+{
+    Node *temp = head;
+    while (temp != nullptr)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+int main()
+{
+
+    Node *head = new Node(10);
+    head->next = new Node(20);
+    head->next->next = new Node(30);
+    head->next->next->next = new Node(40);
+
+    cout << "Linked List: ";
+    print(head);
+
+    return 0;
+}
